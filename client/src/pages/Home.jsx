29f4df1a3fb2332a789e9e05445ed52bae6a5644
GoldBerg@12 <vignesh772001@gmail.com>
2024-09-03 22:14:52 +0530

    Changes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import Github from "../assets/Github.png"
 
 const Home = () => {
@@ -9,8 +8,8 @@ const Home = () => {
           Welcome to my Authenticator App!
         </h1>
         <p className="mb-4 text-lg text-justify font-main font-medium text-slate-700">
-          I'm glad that you have visited my app, Down here is my Github Profile
-          where I'll be posting web dev contents daily and other technology
+          I&apos;m glad that you have visited my app, Down here is my Github Profile
+          where I&apos;ll be posting web dev contents daily and other technology
           related concepts
           <br />
           Do Follow me!
@@ -18,7 +17,7 @@ const Home = () => {
       </div>
       <div className="flex flex-col justify-center items-center mt-3">
         <h1>My Github</h1>
-        <a href="https://github.com/vignesh7701/" target="_blank">
+        <a href="https://github.com/vignesh7701/" target="_blank" rel="noreferrer">
           <img className='w-24' src={Github} alt="Github" />
         </a>
       </div>
